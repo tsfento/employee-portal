07ddaf0a7782d771978702183a60dda319ad9180
Tyler Fenton <tsfento@gmail.com>
2023-12-04 12:34:16 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     LandingPageComponent,
-    AuthComponent
+    AuthComponent,
     SidebarComponent,
     ContainerComponent
   ],
