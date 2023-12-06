9a2d221c37a58de734c4096520622beb3948a6b5
Tyler Fenton <tsfento@gmail.com>
2023-12-06 15:05:07 -0600

    Revert "Bug sidebar company name fenton"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { AuthComponent } from './components/auth/auth.component';
-import { ContainerComponent } from './components/container/container.component';
 
-const routes: Routes = [
-
-  { path: '', component: LandingPageComponent },
-  { path: 'landing', component: LandingPageComponent },
-  { path: 'auth', component: AuthComponent },
-  { path: 'user', component: ContainerComponent }
- 
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { AuthComponent } from './components/auth/auth.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ContainerComponent } from './components/container/container.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LandingPageComponent,
-    AuthComponent,
-    SidebarComponent,
-    ContainerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
