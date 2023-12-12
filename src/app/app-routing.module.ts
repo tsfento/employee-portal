8d35a52391fcdd4a268aab28b520ddb25413aaa5
Tyler Fenton <tsfento@gmail.com>
2023-12-12 13:19:33 -0600

    Working on org-chart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,20 @@ import { ContainerComponent } from './components/container/container.component';
 import { PersonnelPageComponent } from './components/personnel-page/personnel-page.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { authGuard } from './components/auth/auth.guard';
+import { OrgChartComponent } from './components/org-chart/org-chart.component';
 
 
 const routes: Routes = [
-
-  { path: '', component: LandingPageComponent, canActivate: [authGuard] },
+  { path: 'welcome', component: LandingPageComponent, canActivate: [authGuard] },
+  { path: '', component: ContainerComponent, canActivate: [authGuard], children: [
+    { path: 'admin', component: AdminComponent },
+    { path: 'personnel', component: PersonnelPageComponent },
+    { path: 'org-chart', component: OrgChartComponent },
+  ] },
+  // { path: 'welcome', component: LandingPageComponent, canActivate: [authGuard] },
   { path: 'landing', component: LandingPageComponent },
   { path: 'auth', component: AuthComponent },
-  { path: 'personnel', component: PersonnelPageComponent, canActivate: [authGuard] },
-  { path: 'user', component: ContainerComponent, canActivate: [authGuard], children: [
-    { path: 'admin', component: AdminComponent }
-  ] },
-
-
+  // { path: 'personnel', component: PersonnelPageComponent, canActivate: [authGuard] },
 ];
 
 @NgModule({
---
+++ src/app/components/org-chart/org-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-org-chart',
+  templateUrl: './org-chart.component.html',
+  styleUrls: ['./org-chart.component.css']
+})
+export class OrgChartComponent {
+
+}
