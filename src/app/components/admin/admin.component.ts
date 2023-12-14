35e8a022f8309237791a07673d0bbf70be72c466
Tyler Fenton <tsfento@gmail.com>
2023-12-14 11:44:57 -0600

    Added updating company name on sidebar when changing in admin

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Subject } from 'rxjs';
 import { Organization } from 'src/app/models/organization.model';
+import { StorageService } from 'src/app/services/storage.service';
 
 
 @Component({
@@ -11,17 +11,13 @@ import { Organization } from 'src/app/models/organization.model';
 })
 export class AdminComponent implements OnInit {
   adminForm: FormGroup;
-  company = new Organization(
-    'Conglomo LLC',
-    '123 Fake St.',
-    'Shermer',
-    'IL',
-    '60062',
-    '555-555-1234'
-  )
-  companyName = new Subject<string>();
+  company: Organization;
+
+  constructor(private storageService: StorageService) {}
 
   ngOnInit() {
+    this.company = this.storageService.getOrganization();
+
     this.adminForm = new FormGroup({
       orgName: new FormControl(this.company.name, Validators.required),
       orgAddress: new FormControl(this.company.address, Validators.required),
@@ -40,7 +36,7 @@ export class AdminComponent implements OnInit {
     this.company.zip = this.adminForm.get('orgZip').value;
     this.company.phone = this.adminForm.get('orgPhone').value;
 
-    this.companyName.next(this.company.name);
+    this.storageService.setOrganization(this.company);
 
     this.onResetForm();
   }
